7507194ba430972c9164fd050eb1a7b39ff69d73
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-12-13 08:50:31 -0500

    Require inputs to be integers.

--- twenty-four.py
+++ twenty-four.py
@@ -8,21 +8,32 @@ ops = {
     "/": operator.div
 }
 
+def enter_integer(order):
+    while True:
+        try:
+            num = int(raw_input('Enter your %s number: '% order))
+        except ValueError:
+            print "Must be an integer."
+            continue
+        else:
+            break
+    return num
+
 print "Welcome to the 24-solver"
-first_num = input('Enter first number: ')
-second_num = input('Enter second number: ')
-third_num = input('Enter third number: ')
-fourth_num = input('Enter fourth number: ')
+first_num = enter_integer('first')
+second_num = enter_integer('second')
+third_num = enter_integer('third')
+fourth_num = enter_integer('fourth')
 print "-----------------------"
 
 nums = [float(first_num), float(second_num), float(third_num), float(fourth_num)]
 num_combos = list(itertools.permutations(nums, 4))
 ops_combos = list(itertools.product(ops, repeat=3))
 results = []
-for num_com in num_combos:
-    for op_com in ops_combos:
+for num_com in num_combos: # Loops through all number permutations (24 possibilities)
+    for op_com in ops_combos: # (Loops through all operator combinations (64 possibilities))
         result = 0
-        try:
+        try: # Moves the parenthesis around in the calculation - probably missing some
             zero_two_one = ops[op_com[1]](ops[op_com[0]](num_com[0], num_com[1]), ops[op_com[2]](num_com[2], num_com[3]))
             if zero_two_one == 24:
                 print "(" + str(int(num_com[0])) + str(op_com[0]) + str(int(num_com[1])) + ")" + str(op_com[1]) + "(" + str(int(num_com[2])) + str(op_com[2]) + str(int(num_com[3])) + ")"
@@ -43,3 +54,7 @@ for num_com in num_combos:
 
 # TODO
 # Validate that inputs are integers
+# Add more cases with parenthesis - probably missing some
+# Add option to filter out negative numbers from calculation.
+# Group similar calculations together?
+# Eliminate calculations that are the same via associate/commutative properties?
